664b57ce244b60ad24e6f85e82f9d704188ca2f7
Jun He <junnhe@gmail.com>
2012-05-16 11:31:44 -0700

    modified convert-trace.sh to take input name from arguments instead of having static filename

--- convert-trace.sh
+++ convert-trace.sh
@@ -1,3 +1,9 @@
-sed -i 's/\.\ /\ /g' trace.txt
-sed -i 's/\[/\ /g' trace.txt
-sed -i 's/\]/\ /g' trace.txt
+#!/bin/bash
+# This script is used to convert a trace (plfs_map output) to the format that I can use in my index-analyzer.
+
+
+echo "File to be converted: $1"
+ifile=$1
+sed -i 's/\.\ /\ /g' $ifile
+sed -i 's/\[/\ /g' $ifile
+sed -i 's/\]/\ /g' $ifile
