88bf7ca255a12e9f565b3d541a497691abadf2bb
Jun He <junnhe@gmail.com>
2012-06-12 03:14:23 -0700

    added afew classes and make all derived from patternabstract

--- patternanalyzer.h
+++ patternanalyzer.h
@@ -1,26 +1,44 @@
 #ifndef __patternanalyzer_h__
 #define __patternanalyzer_h__
 
+#include <string>
+
 namespace MultiLevel {
 
-    class PatternUnit
+    class PatternAbstract {
+        virtual string show() = 0;
+    }
+
+    class PatternUnit: public PatternAbstract
     {
         off_t init;
         vector<off_t> seq;
         int cnt;
     };
 
-    class PatternBlock 
+    class PatternBlock: public PatternAbstract 
     {
         vector<PatternUnit> blocks;
     };
 
-    class PatternChunk
+    class PatternChunk: public PatternAbstract
     {
         vector<PatternBlock> chunks;
     };
     
+    class PatternEntry: public PatternAbstract
+    {
+        pid_t original_chunk_id;
+        pid_t new_chunk_id;
+        PatternChunk logical_offset;
+        PatternChunk length;
+        PatternChunk physical_offset;
+    };
 
+    class PatternList: public PatternAbstract
+    {
+        vector<PatternEntry> list;
+    }
 }
 
 
