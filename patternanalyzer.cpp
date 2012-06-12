207410b3ff81e1f55f9b3e78541e5e713e9324d0
Jun He <junnhe@gmail.com>
2012-06-12 04:49:15 -0700

    added buildDeltaBlock, added show() for pattern head unit

---
+++ patternanalyzer.cpp
@@ -0,0 +1,56 @@
+
+#include "patternanalyzer.h"
+
+namespace MultiLevel {
+   
+   PatternBlock buildDeltaBlock( PatternBlock &pblock )
+   {
+       vector<PatternHeadUnit>::iterator it;
+       PatternBlock deltas;
+       for ( it = pblock.block.begin();
+             it != pblock.block.end();
+             it++ ) 
+       {
+           if ( it != pblock.block.begin() ) {
+               PatternHeadUnit phu;
+               phu.init = it->init - (it-1)->init;
+               deltas.block.push_back( phu );
+           }
+       }
+       return deltas;
+   }
+
+
+
+   // input: many PatternUnit (just offset, or PatternHeadUnit)
+   // output: a PatternChunk describing the pattern:
+   //            with two blocks if pattern found
+   //            with only one if not found
+   PatternChunk PatternAnalyzer::generatePatterns( PatternBlock &pblock )
+   {
+
+   }
+
+
+   string PatternHeadUnit::show()
+   {
+       ostringstream oss;
+       oss << init << ",";
+       oss << "(";
+       vector<off_t>::iterator it;
+       for ( it = seq.begin();
+             it != seq.end();
+             it++ )
+       {
+           oss << *it ;
+           if ( it + 1 != seq.end() ) {
+               // not the last one
+               oss << ","
+           }
+       }
+       oss << ")";
+       return oss.str();
+   }
+}
+
+
--- patternanalyzer.h
+++ patternanalyzer.h
@@ -2,43 +2,85 @@
 #define __patternanalyzer_h__
 
 #include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
 
 namespace MultiLevel {
 
     class PatternAbstract {
+        /*
         virtual string show() = 0;
-    }
+        virtual bool follows( PatternAbstract &other ) = 0;
+        virtual string serialize() = 0;
+        virtual string deSerialize() = 0;
+        */
+    };
+
+    ///////////////////////////////////////////////////////////
+    // Pattern Unit
+    ///////////////////////////////////////////////////////////
+    class PatternUnitAbstract: public PatternAbstract
+    {
+    };
 
     class PatternUnit: public PatternAbstract
     {
-        off_t init;
-        vector<off_t> seq;
-        int cnt;
+        public:
+            vector<off_t> seq;
+            int cnt;
     };
+
+    class PatternHeadUnit: public PatternUnit
+    {
+        public:
+            off_t init;
+            string show();
+    };
+
+    class PatternInitUnit: public PatternUnitAbstract
+    {
+        public:
+            off_t init;
+    };
+
+    ///////////////////////////////////////////////////////////
 
     class PatternBlock: public PatternAbstract 
     {
-        vector<PatternUnit> blocks;
+        public:
+            vector<PatternHeadUnit> block;
     };
 
     class PatternChunk: public PatternAbstract
     {
-        vector<PatternBlock> chunks;
+        public:
+            vector<PatternBlock> chunks;
     };
     
     class PatternEntry: public PatternAbstract
     {
-        pid_t original_chunk_id;
-        pid_t new_chunk_id;
-        PatternChunk logical_offset;
-        PatternChunk length;
-        PatternChunk physical_offset;
+        public:
+            pid_t original_chunk_id;
+            pid_t new_chunk_id;
+            PatternChunk logical_offset;
+            PatternChunk length;
+            PatternChunk physical_offset;
     };
 
     class PatternList: public PatternAbstract
     {
-        vector<PatternEntry> list;
-    }
+        public:
+            vector<PatternEntry> list;
+    };
+
+    class PatternAnalyzer 
+    {
+        public:
+            PatternChunk generatePatterns( PatternBlock &pblock );
+    };
+
 }
 
 
